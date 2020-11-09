a02bf2f5bc49ad986210d8d9c3e2564f41918102
Mateus Andriola <mateusandriola18@gmail.com>
2020-11-09 19:16:33 -0300

    Feature: Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Document, { DocumentContext, DocumentInitialProps, Html, Main, NextScript, Head } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import favicon16 from "../assets/favicon-16x16.png";
+import favicon32 from "../assets/favicon-32x32.png";
+import favicon96 from "../assets/favicon-96x96.png";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -34,6 +37,9 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+          <link rel="icon" type="image/png" sizes="96x96" href={favicon96} />
         </Head>
         <body>
           <Main />
